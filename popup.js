4ff967bfd647fb940b91a5954640428d865b729e
diwakarjdr <51310464+diwakarjdr@users.noreply.github.com>
2019-06-03 16:07:37 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
 var r=document.getElementById('result');
 function startlistening(){
     if('webkitSpeechRecognition' in window){
-       var speechRecogniser=new webkitSpeechRecognition();
+       var speechRecogniser=new SpeechRecognition();
        speechRecogniser.continuous=true;
        speechRecogniser.interimResults=true;
-       speechRecogniser.lang='en-IN';
-       speechRecogniser.start();
+       speechRecogniser.maxAlternatives = 1;
+       var speechRecognitionList = new SpeechGrammarList();
+       speechRecognitionList.addFromString(grammar, 1);
+       speechRecogniser.grammars = speechRecognitionList;
+       speechRecogniser.lang='en-US';
        var finalTranscripts='';
-       speechRecogniser.onresult=function(event)
+       /*speechRecogniser.onresult=function(event)
        {  var interimTranscript='';
            for(var i=event.resultIndex;i<event.results.length;i++)
            {
@@ -20,12 +30,36 @@ function startlistening(){
                }
            }
            r.innerHTML=finalTranscripts+ '<span style = "color:#999">'+ interimTranscript + '</span>';
-       };
+       };*/
+       speechRecogniser.onaudiostart = function() {
+           console.log("Audio started");
+       }
+
+       speechRecogniser.onsoundstart = function() {
+          console.log("Sound started");
+       }
+
+       speechRecogniser.onspeechstart = function() {
+           console.log("Speech started");
+       }
+
+       speechRecogniser.onstart = function() {
+          console.log("Speech recognition started");
+       }
+
+       speechRecogniser.onspeechend = function() {
+        speechRecogniser.stop();
+      }
+      
+      speechRecogniser.onnomatch = function(event) {
+        console.log("I didn't recognise that color.");
+      }
        
 
     speechRecogniser.onerror=function(event){
         r.innerHTML.value='error';
     };
+    speechRecogniser.start();
 }
 else{
     r.innerHTML='browser not supported';
