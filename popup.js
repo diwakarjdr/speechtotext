19708fff3c7b5c640a0380abaabaf71bf0be7a89
diwakarjdr <51310464+diwakarjdr@users.noreply.github.com>
2019-06-04 09:56:55 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -55,9 +55,9 @@ recognition.onspeechend = function() {
 
 /*recognition.onnomatch = function(event) {
   diagnostic.textContent = "I didn't recognise that color.";
-}
+}*/
 
 recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
 }
-*/
+
