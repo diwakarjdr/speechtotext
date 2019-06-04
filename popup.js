cf88d7b9d746eb6376576d39b9d7f21fa16a6be5
diwakarjdr <51310464+diwakarjdr@users.noreply.github.com>
2019-06-04 14:46:00 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,87 +1,24 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-
-//var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-//var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
-var grammar= '#JSGF v1.0';
-
-var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
-var hints = document.querySelector('.hints');
-
-/*var colorHTML= '';
-colors.forEach(function(v, i, a){
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
-});*/
-hints.innerHTML = 'Tap/click and say a word';
-
-document.body.onclick = function() {
-  recognition.start();
-  console.log('Ready to receive a color command.');
+var recogniser=new webkitSpeechRecognition();
+window.onload=function(){
+var startbtn=document.getElementById('start');
+var stopbtn=document.getElementById('stop');
+var input=document.getElementById('result');
+recogniser.continuous=true;
+startbtn.onclick=function startlistening(){
+    recogniser.start();
+    
 }
-
-/*recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The [last] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
-
-  var last = event.results.length - 1;
-  var color = event.results[last][0].transcript;
-
-  diagnostic.textContent = 'Result received: ' + color + '.';
- // bg.style.backgroundColor = color;
-  console.log('Confidence: ' + event.results[0][0].confidence);
-}*/
-if('SpeechRecogniser.onresult')
-{
-recognition.addEventListener('result',(event)=>{ 
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The [last] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
-
-  var last = event.results.length - 1;
-  var color = event.results[last][0].transcript;
-
-  diagnostic.textContent = 'Result received: ' + color + '.';
- // bg.style.backgroundColor = color;
-  console.log('Confidence: ' + event.results[0][0].confidence);
+recogniser.onresult=function(event){
+   input.innerHTML=event.results[event.result_index][0].transcript;
+}
+stopbtn.onclick=function stoplistening(){
+   recogniser.stop();
+}
+}
+chrome.runtime.onInstalled.addListener((details) => {
+    
+    chrome.tabs.create({
+        url: chrome.extension.getURL("welcome.html"),
+        active: true
+    })
 })
-
-recognition.onspeechend = function() {
-  recognition.stop();
-}
-
-/*recognition.onnomatch = function(event) {
-  diagnostic.textContent = "I didn't recognise that color.";
-}*/
-
-recognition.onerror = function(event) {
- console.log('error event');
-}
-}
-else
-{
-  console.log('not compatible');
-}
-
